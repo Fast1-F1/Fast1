a52bc825f9f02991caa8faf5182a76a1204870c5
anilyavas <anil.yavas99@gmail.com>
2024-10-07 16:29:06 +0300

    rendering twitter feed of f1

--- app/(tabs)/social.tsx
+++ app/(tabs)/social.tsx
@@ -1,5 +1,21 @@
 import { WebView } from 'react-native-webview';
 
 export default function SocialFeed() {
-  return <WebView className="flex-1" source={{ uri: 'https://x.com/F1' }} />;
+  const twitterAccount = 'F1';
+
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </head>
+      <body style="margin:0;padding:0;">
+        <a class="twitter-timeline" data-theme="light" data-chrome="noheader nofooter noborders" href="https://twitter.com/${twitterAccount}">
+          Tweets by ${twitterAccount}
+        </a> 
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      </body>
+    </html>
+  `;
+  return <WebView className="flex-1" source={{ html: htmlContent }} />;
 }
