60b3755ab47ec384c58850effb44ba31446e6319
anilyavas <anil.yavas99@gmail.com>
2024-10-10 16:41:00 +0300

    auth screen prototype

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,6 +1,8 @@
 import { Stack } from 'expo-router';
 import React, { useState } from 'react';
-import { Alert, View, Button, TextInput } from 'react-native';
+import { Alert, View, Image, TextInput, Pressable, Text, ImageBackground } from 'react-native';
+
+import bg from '../../assets/bg.jpg';
 
 import { supabase } from '~/utils/supabase';
 
@@ -36,39 +38,54 @@ export default function Login() {
   }
 
   return (
-    <View className="flex-1 items-center gap-2 bg-white p-3">
-      <Stack.Screen
-        options={{
-          title: 'Authentication',
-          headerStyle: { backgroundColor: '#FF1E00' },
-          headerTitleStyle: { color: 'white', fontWeight: 'bold', fontSize: 20 },
-          headerTintColor: 'white',
-          headerBackTitle: 'Back',
-        }}
-      />
-      <View className="rounded-lg border border-gray-300 p-2">
-        <TextInput
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize="none"
+    <ImageBackground source={bg} resizeMode="cover" className="h-full w-full">
+      <View className="mb-20 mt-3 items-center p-3">
+        <Text className="text-4xl font-bold text-yellow-400">Welcome to Fast1!</Text>
+      </View>
+      <View className="flex-1 gap-2 p-3">
+        <Stack.Screen
+          options={{
+            title: 'Authentication',
+            headerStyle: { backgroundColor: '#FF1E00' },
+            headerTitleStyle: { color: 'white', fontWeight: 'bold', fontSize: 20 },
+            headerTintColor: 'white',
+            headerBackTitle: 'Back',
+          }}
         />
+        <View className="p-2">
+          <TextInput
+            className="rounded border border-gray-600 bg-white p-5"
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            placeholder="email@address.com"
+            autoCapitalize="none"
+          />
+        </View>
+        <View className="p-2">
+          <TextInput
+            className="rounded border border-gray-600 bg-white p-5"
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+            secureTextEntry
+            placeholder="Password"
+            autoCapitalize="none"
+          />
+        </View>
+        <View className="mb-auto mt-5 gap-3">
+          <Pressable
+            className="items-center rounded-lg border border-gray-400 bg-white p-3"
+            disabled={loading}
+            onPress={() => signInWithEmail()}>
+            <Text className="text-lg font-bold text-[#ff1e00]">Sign In</Text>
+          </Pressable>
+          <Pressable
+            className="items-center rounded-lg bg-[#FF1E00] p-3"
+            disabled={loading}
+            onPress={() => signUpWithEmail()}>
+            <Text className="text-lg font-bold text-white">Sign Up</Text>
+          </Pressable>
+        </View>
       </View>
-      <View>
-        <TextInput
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry
-          placeholder="Password"
-          autoCapitalize="none"
-        />
-      </View>
-      <View>
-        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
-      </View>
-      <View>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
-      </View>
-    </View>
+    </ImageBackground>
   );
 }
