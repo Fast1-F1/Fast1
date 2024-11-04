8652081e925c2b0ea19e365ca389cd94b9620e73
anilyavas <anil.yavas99@gmail.com>
2024-11-04 22:14:39 +0300

    fix file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,15 @@ import { Stack } from 'expo-router';
 
 import AuthContextProvider from '~/context/AuthContext';
 import '../global.css';
-import { NotificationProvider } from '~/context/NotificationProvider';
 
 export default function RootLayout() {
   return (
     <AuthContextProvider>
-      <NotificationProvider>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        </Stack>
-      </NotificationProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
     </AuthContextProvider>
   );
 }
