b4ac6464886dda2e9bf381f4220551361ed6ea64
anilyavas <anil.yavas99@gmail.com>
2024-12-15 21:26:00 +0300

    Wiki loading animation before renders

--- app/wiki/[url].tsx
+++ app/wiki/[url].tsx
@@ -1,11 +1,33 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { Animated, View, Alert, Text } from 'react-native';
 import WebView from 'react-native-webview';
 
+import Loading from '~/components/Loading';
+
 export default function DriverWiki() {
+  const [loading, setLoading] = useState(true);
+  const fadeAnim = useState(new Animated.Value(0))[0];
   const { url } = useLocalSearchParams<string | any>();
+
+  const handleLoadEnd = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    setLoading(false);
+  };
+
+  if (!url) {
+    Alert.alert('Error', 'The URL is missing or invalid.');
+    return null;
+  }
+
   return (
     <View className="flex-1 bg-[#11100f]">
+      {/* Header */}
       <Stack.Screen
         options={{
           title: 'Driver Wiki',
@@ -14,7 +36,28 @@ export default function DriverWiki() {
           headerTintColor: 'white',
         }}
       />
-      <WebView source={{ uri: url }} style={{ flex: 1 }} />
+
+      {/* Conditional rendering for Loading or WebView */}
+      {loading ? (
+        <View className="flex-1">
+          <Loading />
+        </View>
+      ) : null}
+
+      <Animated.View
+        className="absolute bottom-0 left-0 right-0 top-0"
+        style={{ opacity: fadeAnim }}>
+        <WebView
+          source={{ uri: decodeURIComponent(url) }}
+          onLoadEnd={handleLoadEnd}
+          startInLoadingState={false}
+          renderError={() => (
+            <View className="flex-1 items-center justify-center bg-[#11100f]">
+              <Text className="text-white">Failed to load page.</Text>
+            </View>
+          )}
+        />
+      </Animated.View>
     </View>
   );
 }
