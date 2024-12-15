27508a07c3d126c4912197286fd8cddc4c5a0287
anilyavas <anil.yavas99@gmail.com>
2024-12-15 18:16:32 +0300

    wiki page

--- app/wiki/[url].tsx
+++ app/wiki/[url].tsx
@@ -1,11 +1,19 @@
-import { useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function DriverWiki() {
   const { url } = useLocalSearchParams<string | any>();
   return (
-    <View>
+    <View className="flex-1 bg-[#11100f]">
+      <Stack.Screen
+        options={{
+          title: 'Driver Wiki',
+          headerBackTitle: 'Back',
+          headerStyle: { backgroundColor: '#FF1E00' },
+          headerTintColor: 'white',
+        }}
+      />
       <WebView source={{ uri: url }} style={{ flex: 1 }} />
     </View>
   );
