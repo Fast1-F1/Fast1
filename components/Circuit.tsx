90cb9da5f470704137081e88b70dadf8a5a0a3b8
anilyavas <anil.yavas99@gmail.com>
2024-12-16 20:02:02 +0300

    Circuit Location component

--- components/Circuit.tsx
+++ components/Circuit.tsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-export default function Circuit({ longitude, latitude }: { longitude: string; latitude: string }) {
+import Loading from './Loading';
+
+export default function Circuit({ circuitId }: { circuitId: string }) {
+  const [circuit, setCircuit] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCircuit = async () => {
+    try {
+      const response = await fetch('https://ergast.com/api/f1/current/circuits.json');
+      const data = await response.json();
+      const circuitData = data.MRData.CircuitTable.Circuits.find(
+        (item: any) => item.circuitId === circuitId
+      );
+      setCircuit(circuitData);
+    } catch (error) {
+      console.log('Error while fetching circuit data', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCircuit();
+  }, [circuitId]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!circuit) {
+    return <Text className="font-semibold text-red-500">No circuit data found!</Text>;
+  }
+
+  const { lat, long } = circuit.Location;
+
   return (
     <MapView
       style={{ flex: 1 }}
       initialRegion={{
-        latitude: parseFloat(latitude),
-        longitude: parseFloat(longitude),
+        latitude: parseFloat(lat),
+        longitude: parseFloat(long),
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}>
-      <Marker coordinate={{ latitude: parseFloat(latitude), longitude: parseFloat(longitude) }} />
+      <Marker coordinate={{ latitude: parseFloat(lat), longitude: parseFloat(long) }} />
     </MapView>
   );
 }
